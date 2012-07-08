35c8545067ffa12f976cf46557b7bde8c6496f9d
tide zyc <tide.zyc@gmail.com>
2012-07-08 22:16:34 +0800

    init resp

---
+++ scripts/common.js
@@ -0,0 +1,7 @@
+function info(url) {
+	window
+			.open(
+					url,
+					'newwindow',
+					'fullscreen=1,top=0,left=0, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no, status=no');
+}
