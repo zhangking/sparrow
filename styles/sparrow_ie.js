626b2e5ca959907e7367dc82acbc485b4f12f4cc
tide zyc <tide.zyc@gmail.com>
2012-07-15 10:13:10 +0800

    update

--- styles/sparrow_ie.js
+++ styles/sparrow_ie.js
@@ -1,4 +1,3 @@
-
 $().ready(function(){
     $('.table tr').hover(function(){
         $(this).addClass('table-hover');
@@ -16,11 +15,4 @@ $().ready(function(){
         $(this).removeClass('btn-white-hover');
     });
     $('.table-striped tr:odd').addClass('table-striped-odd');
-});
-$(function(){
-    if (window.PIE) {
-        $('.btn').each(function(){
-            PIE.attach(this);
-        });
-    }
 });
